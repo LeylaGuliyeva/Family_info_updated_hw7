e3b61cc8a20c82f293b96e110c7e0cb1500fe96f
LeylaGuliyeva <108999395+LeylaGuliyeva@users.noreply.github.com>
2022-08-26 11:38:32 +0400

    Update Woman.java

--- src/main/java/Hw7/Woman.java
+++ src/main/java/Hw7/Woman.java
@@ -1,4 +1,24 @@
 package Hw7;
 
-public class Woman {
+final public class Woman extends Human {
+    public Woman() {
+        super();
+    }
+    public Woman(String name, String surname, int year) {
+        super(name, surname, year);
+    }
+
+    public Woman(String name, String surname, int year, byte iq, String schedule[][],Family fam, Pet pet) {
+        super(name, surname, year, iq, schedule,fam, pet);
+    }
+
+
+    @Override
+    public void greetPet() {
+        System.out.printf("Hi my dear friend :  %s\n", this.pet);
+    }
+
+    void morning() {
+        System.out.println("Good morning my dear family!!!");
+    }
 }
