bbceca4f430be10124769afbf5af824b2f42ae9e
LeylaGuliyeva <108999395+LeylaGuliyeva@users.noreply.github.com>
2022-08-26 11:43:19 +0400

    Update DomesticCat.java

--- src/main/java/Hw7/DomesticCat.java
+++ src/main/java/Hw7/DomesticCat.java
@@ -1,4 +1,23 @@
 package Hw7;
 
-public class DomesticCat {
+public class DomesticCat extends Pet implements Foulable {
+    public DomesticCat() {
+        super();
+    }
+    public DomesticCat(String nickname) {
+        super(nickname);
+    }
+    public DomesticCat(String nickname, int age, int trickLevel, String habits[]) {
+        super(nickname, age, trickLevel, habits);
+    }
+
+    @Override
+    public void respond() {
+        System.out.println("Hi my dear friend, my name is "+this.nickname+" and I am a cat");
+    }
+
+    @Override
+    public void foul() {
+        System.out.println("It is time to suppress");
+    }
 }
