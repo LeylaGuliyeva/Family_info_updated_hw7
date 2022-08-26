6ad0773377ff8de096757d9b6ad4693e6183eb21
LeylaGuliyeva <108999395+LeylaGuliyeva@users.noreply.github.com>
2022-08-26 11:42:17 +0400

    Update Fish.java

--- src/main/java/Hw7/Fish.java
+++ src/main/java/Hw7/Fish.java
@@ -1,4 +1,20 @@
 package Hw7;
 
-public class Fish {
-}
+public class Fish extends Pet implements Foulable {
+    public Fish() {
+        super();
+    }
+    public Fish(String nickname) {
+        super(nickname);}
+
+    public Fish(String nickname, int age, int trickLevel, String habits[]) {
+            super(nickname, age, trickLevel, habits);
+        }
+
+        @Override
+        public void respond() {
+            System.out.println("Hi my dear friend, my name is "+this.nickname+" and I am a fish");
+        }
+
+
+    }
