450c3278f092d5cc02c139cd8a481704db7f4da5
LeylaGuliyeva <108999395+LeylaGuliyeva@users.noreply.github.com>
2022-08-26 11:33:14 +0400

    Update Man.java

--- src/main/java/Hw7/Man.java
+++ src/main/java/Hw7/Man.java
@@ -1,4 +1,27 @@
 package Hw7;
 
-public class Man {
+import java.sql.SQLOutput;
+
+final public class Man extends Human {
+
+    public Man(String name, String surname, int year) {
+        super(name, surname, year);
+    }
+
+    public Man(String name, String surname, int year, byte iq, String schedule[][],Family fam, Pet pet) {
+        super(name, surname, year, iq, schedule,fam, pet);
+    }
+
+    public Man() {
+        super();
+    }
+
+    @Override
+    public void greetPet() {
+        System.out.printf("Hi buddy :  %s\n", this.pet);
+    }
+
+    void football() {
+        System.out.println("My favourite team is Real Madrid");
+    }
 }
