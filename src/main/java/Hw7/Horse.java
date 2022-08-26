da637ecc0bead5c82a48d99b9da4c39042c0cf1a
LeylaGuliyeva <108999395+LeylaGuliyeva@users.noreply.github.com>
2022-08-26 11:41:23 +0400

    Update Horse.java

--- src/main/java/Hw7/Horse.java
+++ src/main/java/Hw7/Horse.java
@@ -1,4 +1,19 @@
 package Hw7;
 
-public class Horse {
-}
+    public class Horse extends Pet {
+        public Horse() {
+            super();
+        }
+        public Horse(String nickname) {
+            super(nickname);
+        }
+        public Horse(String nickname, int age, int trickLevel, String habits[]) {
+            super(nickname, age, trickLevel, habits);
+        }
+
+        public void respond() {
+            System.out.println("Hi my dear friend, my name is "+this.nickname+" and I am a horse");
+        }
+
+    }
+
