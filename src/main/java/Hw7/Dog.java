545d8e9dc95729c0dcb1b7e33ef785a64b964c42
LeylaGuliyeva <108999395+LeylaGuliyeva@users.noreply.github.com>
2022-08-26 11:43:40 +0400

    Update Dog.java

--- src/main/java/Hw7/Dog.java
+++ src/main/java/Hw7/Dog.java
@@ -1,4 +1,24 @@
 package Hw7;
 
-public class Dog {
+public class Dog extends Pet implements Foulable {
+    public Dog() {
+        super();
+    }
+    public Dog(String nickname){
+        super(nickname);}
+
+    public Dog(String nickname, int age, int trickLevel, String habits[]) {
+        super(nickname, age, trickLevel, habits);
+    }
+
+    @Override
+    public void respond() {
+        System.out.println("Hi my dear friend, my name is "+this.nickname+" and I am a dog");
+    }
+
+    @Override
+    public void foul() {
+        System.out.println("It is time to suppress");
+    }
+
 }
