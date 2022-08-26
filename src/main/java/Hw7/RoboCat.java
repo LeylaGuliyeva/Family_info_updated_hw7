0bc2fff19a3641b12a27d5f87128602f7eb3d23e
LeylaGuliyeva <108999395+LeylaGuliyeva@users.noreply.github.com>
2022-08-26 11:37:26 +0400

    Update RoboCat.java

--- src/main/java/Hw7/RoboCat.java
+++ src/main/java/Hw7/RoboCat.java
@@ -1,4 +1,26 @@
 package Hw7;
 
-public class RoboCat {
+
+public class RoboCat extends Pet implements Foulable {
+    public RoboCat() {
+        super();
+    }
+    public RoboCat(String nickname) {
+        super(nickname);
+    }
+
+    public RoboCat(String nickname, int age, int trickLevel, String habits[]) {
+        super(nickname, age, trickLevel, habits);
+    }
+
+
+    @Override
+    public void respond() {
+        System.out.println("Hi my dear friend, my name is "+this.nickname+" and I am a robo cat");
+    }
+
+    @Override
+    public void foul() {
+        System.out.println("It is time to suppress");
+    }
 }
