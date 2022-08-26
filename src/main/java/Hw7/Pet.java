79de5579d2afe57e73f5339cfbd9cd8010180668
LeylaGuliyeva <108999395+LeylaGuliyeva@users.noreply.github.com>
2022-08-26 11:36:53 +0400

    Update Pet.java

--- src/main/java/Hw7/Pet.java
+++ src/main/java/Hw7/Pet.java
@@ -1,4 +1,112 @@
 package Hw7;
 
-public class Pet {
+
+import java.util.Arrays;
+import java.util.Objects;
+
+
+public abstract class Pet {
+
+    protected Species species;
+    protected String nickname;
+    protected int age;
+    protected int trickLevel;
+    protected String habits[];
+    public Species getSpecies() {
+        return species;
+    }
+    public void setSpecies(Species species) {
+        this.species = species;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getTrickLevel() {
+        return trickLevel;
+    }
+
+    public void setTrickLevel(int trickLevel) {
+        this.trickLevel = trickLevel;
+    }
+
+    public String[] getHabits() {
+        return habits;
+    }
+
+    public void setHabits(String[] habits) {
+        this.habits = habits;
+    }
+
+    final void eat(){
+        System.out.println("I am eating");
+    }
+    public void respond(){
+        System.out.println("Hello, friend");
+    }
+    public void foul(){
+        System.out.println("I need to cover it up.");
+    }
+
+    @Override
+    public String toString() {
+        return this.species + "{nickname='" + this.nickname + "', age=" + this.age + ", trickLevel=" + this.trickLevel + ", habits=" + Arrays.toString(this.habits) + "}";
+    }
+
+    protected Pet(){
+    }
+    protected Pet(String nnm){
+        this.nickname=nnm;
+
+    }
+    protected Pet(String nnm,int a,int tl,String hbt[]){
+        this.nickname=nnm;
+        this.age=a;
+        this.trickLevel=tl;
+        this.habits=hbt;
+    }
+
+    {
+        for(Species sp: Species.values()) {
+            if(sp.toString().toUpperCase().equals(this.getClass().getSimpleName().toUpperCase()))
+                this.species = sp;
+        }
+
+        if(this.species == null)
+            this.species = Species.UNKNOWN;
+    }
+
+
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pet)) return false;
+        Pet pet = (Pet) o;
+        return getAge() == pet.getAge() && getSpecies().equals(pet.getSpecies()) && getNickname().equals(pet.getNickname());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getSpecies(), getNickname(), getAge());
+    }
+    @Override
+    protected void finalize(){
+        System.out.println("Pet class element will be deleted!");
+    }
 }
